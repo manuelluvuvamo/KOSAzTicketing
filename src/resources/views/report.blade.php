d610ce3f4530c4201d812c1fe80717dc6f021946
manuelluvuvamo <manuelluvuvamo337@gmail.com>
2025-01-28 10:23:00 +0100

    Finalização do report de erros

--- src/resources/views/report.blade.php
+++ src/resources/views/report.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reportar um problema</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <style>
         body {
             background-color: #f8f9fa;
@@ -94,7 +93,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    
+
 </body>
 
 
